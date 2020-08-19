f593ede2659db11f094f3560bc948f8d31f4a4cc
TwoCatsCanFly <TwoCatsCanFly@users.noreply.github.com>
2020-08-19 10:40:52 +0300

    Password success

--- auth_enj/urls.py
+++ auth_enj/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import *
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('edit-profile/', UserEditView.as_view(), name='edit_profile'),
+    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
+    path('password_success/', views.password_success, name='password_success'),
 ]
+
