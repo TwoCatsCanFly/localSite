e3ee22a78d44be32718b8077623c06cd56425d49
TwoCatsCanFly <TwoCatsCanFly@users.noreply.github.com>
2020-08-25 10:53:08 +0300

    Social links

--- blog/models.py
+++ blog/models.py
@@ -36,3 +36,17 @@ class Post(models.Model):
 
 
     # input_formats='%H:%M:%S %d %B %Y'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    pinterest_url = models.CharField(max_length=255, null=True, blank=True)
+
+
+    def __str__(self):
+        return str(self.user)
